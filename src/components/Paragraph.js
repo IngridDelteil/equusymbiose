a65da16d36833584d471af48f90c5de658fe2683
Ingrid Delteil <ingrid.delteil@gmail.com>
2023-02-13 11:39:32 +0100

    Finalisation de la page Libération Myofasciale

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -5,11 +5,12 @@ const Paragraph = ({
   alignment = "justify",
   color = "primary.dark",
   fontWeight = "400",
+  fontStyle = "normal",
 }) => {
   return (
     <Typography
       color={color}
-      sx={{textAlign: alignment, fontWeight: fontWeight}}
+      sx={{textAlign: alignment, fontWeight: fontWeight, fontStyle: fontStyle}}
     >
       {children}
     </Typography>
--- src/components/SubTitle.js
+++ src/components/SubTitle.js
@@ -1,20 +1,56 @@
 import {Typography} from "@mui/material";
 
-const SubTitle = ({children}) => {
-  return (
-    <Typography
-      component='h2'
-      variant='h6'
-      color='secondary.main'
-      sx={{
-        textTransform: "uppercase",
-        fontWeight: 500,
-        mb: 2,
-      }}
-    >
-      {children}
-    </Typography>
-  );
+const SubTitle = ({children, level = 1}) => {
+  switch (level) {
+    case 2: {
+      return (
+        <Typography
+          component='h3'
+          variant='subtitle1'
+          color='error.main'
+          sx={{
+            textTransform: "uppercase",
+            fontWeight: 500,
+          }}
+        >
+          {children}
+        </Typography>
+      );
+    }
+    case 3: {
+      return (
+        <Typography
+          component='h3'
+          variant='body2'
+          color='error.main'
+          sx={{
+            textTransform: "uppercase",
+            fontWeight: 500,
+            textDecoration: "underline",
+          }}
+        >
+          {children}
+        </Typography>
+      );
+    }
+    case 1:
+    default: {
+      return (
+        <Typography
+          component='h2'
+          variant='h6'
+          color='secondary.main'
+          sx={{
+            textTransform: "uppercase",
+            fontWeight: 500,
+            mb: 2,
+          }}
+        >
+          {children}
+        </Typography>
+      );
+    }
+  }
 };
 
 export default SubTitle;
